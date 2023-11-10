d4efa41cb61d9143cdabb2a11f63faf721560d9f
httpscammy <109007099+httpscammy@users.noreply.github.com>
2023-11-10 22:47:37 +0000

    First commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,71 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, {useEffect, useState} from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrashCan } from '@fortawesome/free-solid-svg-icons'
 
 //create your first component
 const Home = () => {
+	const [todo, setTodo] = useState('')
+	const [todos, setTodos] = useState([])
+	const handleClick = () => {
+		setTodos(prev => [...prev, todo])
+		setTodo('')
+	}
+	
+	useEffect (() => {
+		if (todos.length > 0) {
+			fetch('https://playground.4geeks.com/apis/fake/todos/user/httpscammy', {
+			method: "PUT",
+			body: JSON.stringify(todos.map(todo => ({label: todo, done: false}))),
+			headers: {
+		  	"Content-Type": "application/json"
+			}
+	  		})
+	  		.catch(error => {//error handling
+		  	console.log(error);
+	  		});
+		}
+		} , [todos])
+
+		window.onload() = () => {
+			fetch('https://playground.4geeks.com/apis/fake/todos/user/httpscammy', {
+			method: "GET",
+			headers: {
+		  	"Content-Type": "application/json"
+			}
+			.then
+		}
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1 className="header">My Todos</h1>
+			<input 
+				type="text" 
+				onChange={(evt) => setTodo(evt.target.value)} 
+				value={todo}
+				onKeyDown={(evt) => {
+					if (evt.key === "Enter") {
+						setTodos(prev => [...prev,todo]); 
+						setTodo("");
+					}
+				}}
+				placeholder="What do you need to do?"  
+			/>
+			
+			<button 
+				onClick ={() => setTodos(prev => [...prev,todo])}>
+					Send
+			</button>
+			<ul>
+				{todos.map((data, index) => 
+					<li key={`${data}-${index}`}>{data} 
+						<FontAwesomeIcon icon={faTrashCan} 
+							onClick= {() => {
+								setTodos(todos.filter((data, currentIndex) => index != currentIndex));
+								}}/>
+					</li>)}
+			</ul>
+			<div>
+				{todos.length} tasks
+			</div>
 		</div>
 	);
 };
